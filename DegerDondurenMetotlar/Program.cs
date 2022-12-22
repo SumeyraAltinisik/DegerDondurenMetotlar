1d5087cd294b96a06bdbe12b022efd6a1a08bf9b
SumeyraAltinisik <sfa2023@gmail.com>
2022-12-22 15:22:38 +0300

    Add project files.

---
+++ DegerDondurenMetotlar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DegerDondurenMetotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int cevap= KareninAlaniniHesapla();
+            Console.WriteLine("Karenin alanı:{0}", cevap);
+            Console.ReadKey();
+        }
+
+        static int KareninAlaniniHesapla()
+        {
+            Console.WriteLine("Hesaplamak istediğiniz karenin uzunluğunu giriniz.");
+            int kenar= Convert.ToInt32(Console.ReadLine());
+
+            int alan = kenar * kenar;
+            return alan;
+        }
+    }
+}
